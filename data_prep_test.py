0458f8d736d40cf0cf824d0ff8c8c0dae1b88e45
sambitbehura <39611727+sambitbehura@users.noreply.github.com>
2018-09-28 22:50:28 +0530

    Update data_prep_test.py

--- data_prep_test.py
+++ data_prep_test.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
  
 mods = ['32PSK','16APSK','32QAM','FM','GMSK','32APSK','OQPSK','8ASK','BPSK','8PSK','AM-SSB-SC','4ASK','16PSK','64APSK','128QAM','128APSK','AM-DSB-SC','AM-SSB-WC','64QAM','QPSK','256QAM','AM-DSB-WC','OOK','16QAM']
-mods_filt=['8PSK','AM-DSB-WC','BPSK','OOK','GMSK','4ASK','16QAM','64QAM','QPSK','FM']
+mods_filt=['8PSK','AM-DSB-WC','BPSK','16APSK','GMSK','4ASK','16QAM','64QAM','QPSK','FM']
 #'CPFSK' is replaced by 'OOK' and 'PAM4' by '4ASK'
 snr_range=[-8,8]
 snrs=np.array(range(snr_range[0],snr_range[1]+1,2))
@@ -28,7 +28,7 @@ for ind in range(0,len(Xd['X'])):
 del Xd
 
 
-data_file = h5py.File('dataset/selected_data.hdf5', 'w')
+data_file = h5py.File('dataset/RML2018_selected_data.hdf5', 'w')
 data_file.create_dataset('data', data=np.array(data))
 data_file.create_dataset('mod_label', data=np.array(mod_label))
 data_file.create_dataset('snr_label', data=np.array(snr_label))
